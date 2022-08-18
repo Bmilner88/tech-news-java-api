5dafda823d6ff4a37f42a8210d1c603ff0da3471
Ben Milner <bmilner88@gmail.com>
2022-08-18 14:29:08 -0700

    Saving Progress
    Demo stuff

---
+++ src/main/java/com/technews/technewsjavaapi/VariableTypes.java
@@ -0,0 +1,16 @@
+package com.technews.technewsjavaapi;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int num1 = 1;
+        double num2 = 2.2;
+        String str1 = "string1";
+
+        double sum1 = num1 + num2;
+
+        String concat = num1 + str1;
+
+        System.out.println(sum1);
+        System.out.println(concat);
+    }
+}
